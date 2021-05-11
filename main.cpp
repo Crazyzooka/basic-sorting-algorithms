5edf4f9c86767ad1e5cb3d179a9905d3bdd09bf7
Crazyzooka <50309931+Crazyzooka@users.noreply.github.com>
2021-05-11 21:36:06 +1000

    Applied changes to main to display more info
    Uses both sorting algorithms.

--- main.cpp
+++ main.cpp
@@ -8,18 +8,34 @@
 
 int main()
 {
-	QuickSort test;
+	BubbleSort sort0;
+	QuickSort sort1;
 	RecursiveBinarySearch search;
-	int arr[8] = { 1, 3, 5, 4, -5, 100, 7777, 2014 };
-	test.sortInt(arr, 0, 8);
+	int arr0[8] = { 1, 3, 5, 4, -5, 100, 7777, 2014 };
+	int arr1[8] = { 1, 3, 5, 4, -5, 100, 7777, 2014 };
 
+	sort0.sortInt(arr0, 0, 8);
+	sort1.sortInt(arr1, 0, 8);
+	
+	std::cout << "Bubblesort: \n";
+	
 	for (int j = 0; j < 8; j++)
 	{
-		std::cout << arr[j] << " ";
+		std::cout << arr0[j] << " ";
 	}
+	
+	std::cout << "\n";
+	
+	std::cout << "QuickSort: \n";
+	
+	for (int j = 0; j < 8; j++)
+	{
+		std::cout << arr1[j] << " ";
+	}
+	
 	std::cout << "\n";
 
-	std::cout << search.searchInt(arr, 0, 8) << "\n";
+	std::cout << search.searchInt(arr0, 0, 8) << "\n";
 
 	return 0;
 }
